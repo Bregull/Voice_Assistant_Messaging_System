088ca9251e68def6586d6f57ec22475c74a8045b
jacek-fica <61587630+jacek-fica@users.noreply.github.com>
2020-04-08 09:15:01 +0200

    Nagrywanie dodane

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def speak(text):
     tts.save(file_name)
     playsound.playsound(file_name)
 
+def record(r, source):
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+    with open('speech.wav', 'wb') as f:
+        f.write(audio.get_wav_data())
+
 
 def get_audio():
     r = sr.Recognizer()
@@ -24,7 +30,8 @@ def get_audio():
                 said = r.recognize_google(audio)
                 if 'hey' in said:
                     print("HI")
-                    playsound.playsound('im listening.mp3')
+                    playsound.playsound('im_listening.mp3')
+                    record(r, source)
                     break
                 else:
                     print('error')
