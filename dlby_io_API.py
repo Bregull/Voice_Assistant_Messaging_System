ab933f07c59c675999f3c68d7b5a99295f53b104
jacek-fica <61587630+jacek-fica@users.noreply.github.com>
2020-05-05 09:05:21 +0200

    Small changes again

--- dlby_io_API.py
+++ dlby_io_API.py
@@ -1,11 +1,53 @@
 import os
+import requests
 import time
 import json
-import requests
 
-# We have different environments but typically you'll
-# always use this endpoint
+
 api_url = 'https://api.dolby.com'
+
+
+def get_upload_url(local_path, dolby_in_url, api_url, api_key):
+    """
+    get_upload_url returns a pre-signed URL for a cloud storage
+    provider such as S3 or GCS where you will be able to PUT
+    the contents of your file
+    """
+    url = api_url + '/media/input'
+
+    # You need your api key as a header
+    headers = {
+        'x-api-key': api_key,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    # This defines a url like dlb://foo that is temporary (~24h)
+    # and only available to you with your api key
+    body = {
+        'url': dolby_in_url
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+
+        return data['url']
+
+    response.raise_for_status()
+
+def upload_file(local_path, upload_url):
+    """
+    upload_file is the actual file upload, reading a local path
+    on disk and putting it to the upload url returned from
+    https://api.dolby.com/file/upload
+    """
+    with open(local_path, 'rb') as file_content:
+        try:
+            requests.put(upload_url, data=file_content)
+        except ValueError as e:
+            print(e)
+            raise
 
 # A limit on how long to poll for results
 MAX_WAIT = 50
@@ -94,13 +136,39 @@ def noise(dolby_in_url, dolby_out_url, api_url, api_key):
     print(json.dumps(data, indent=4, sort_keys=True))
 
 
+def download(dolby_out_url, api_url, api_key, new_file_name):
+
+    url = api_url + '/media/output' + '?url=' + dolby_out_url
+    headers = {
+        'x-api-key': api_key,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    response = requests.get(url, headers=headers, stream=True, allow_redirects=True)  # to get content after redirection
+    print(response)
+    if response.status_code == 200:
+        with open(new_file_name, 'wb') as f:
+            f.write(response.content)
 
 
-# The api_key from your account, dolby dlb:// url created in an earlier step, and
-# a dolby dlb:// url that will created on-the-fly to output your result
+# This is the function which includes every step
+def dlby_API(local_path, dolby_in_url, dolby_out_url, api_key, new_file_name):
+    #upload
+    upload_url = get_upload_url(local_path, dolby_in_url, api_url, api_key)
+    upload_file(local_path, upload_url)
+    print("You can now use {} as your input for Media APIs.".format(dolby_in_url))
+    #enhance
+    noise(dolby_in_url, dolby_out_url, api_url, api_key)
+    print("If successful, you can call /media/output with {} to download and hear the results.".format(dolby_out_url))
+    #download
+    download(dolby_out_url,api_url, api_key, new_file_name)
+
+# The api_key from your account, local_path to your file, a dolby_url of your choosing
 api_key = 'b6tGyxR4AuO0CWefbrMyBwJ0fBBcYS81'
+local_path = 'police_car.wav'
 dolby_in_url = 'dlb://input.wav'
 dolby_out_url = 'dlb://output.wav'
+new_file_name = 'new_police_car1.wav'
 
-noise(dolby_in_url, dolby_out_url, api_url, api_key)
-print("If successful, you can call /media/output with {} to download and hear the results.".format(dolby_out_url))
+dlby_API(local_path, dolby_in_url, dolby_out_url, api_key, new_file_name)
